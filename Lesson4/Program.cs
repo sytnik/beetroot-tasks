36c00eb983759d1dfc3cee7a6f56a09743552cb6
Oleh Sytnik <osyt@telenor.dk>
2022-10-14 21:25:07 +0300

    Update Program.cs

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -3,7 +3,8 @@
 //Console.WriteLine($"Result {result}");
 
 int? int1 = 5;
-List<int> list = new List<int>();
+List<int> list = new List<int> {1, 34, 5, 4};
+list.Remove(34);
 int result = 0;
 Console.WriteLine($"Result {new TestClass().Method12(ref int1, 7)}");
 Console.WriteLine($"Result {new TestClass().Method12(ref int1, 7)}");
@@ -11,8 +12,34 @@ IncrementList(ref result, list, 1, 2, 3, 4, 5, 6);
 Console.WriteLine($"Result: {string.Join(",", list)}");
 ReqMethod(5);
 var str = new TestClass().Method12(ref int1, 2);
-
 Console.WriteLine(str);
+var myarray = new[] {1, 2, 3};
+Array.Resize(ref myarray, 10);
+for (int i = 3; i < myarray.Length; i++)
+{
+    myarray[i] = i + 1;
+}
+
+myarray = myarray.Where(value => value % 2 == 0 && value > 2).ToArray();
+var test = myarray.Where(value => value != 7);
+var strArray = new string[3];
+strArray[0] = "12345";
+strArray[1] = "574578";
+strArray[2] = "   464778    ";
+var matches =
+    strArray
+        .Where(s => s.Trim().StartsWith("46"))
+        .Select(s => s.Trim())
+        .ToList();
+var test1 = string.IsNullOrEmpty(strArray[2]);
+var test2 = string.IsNullOrWhiteSpace(strArray[2]);
+Console.WriteLine(str);
+
+string ConcatString(string param1, string param2)
+{
+    return param1 + " " + param2;
+}
+
 
 int ReqMethod(int param)
 {
